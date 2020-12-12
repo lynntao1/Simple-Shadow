dbc69c61943db9c47fc5e217d04c4314f9f54758
Jawand <63509680+JawandS@users.noreply.github.com>
2020-12-12 11:36:56 -0500

    Added test

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test{
+
+    int key = 0;
+    int pass = 1454;
+
+    public test(int k){
+        key = k;
+    }
+
+    public int getKey(int p){
+        if(p==pass)
+            return key;
+
+        return 0000;
+    }
+}

e39159dcf37bb083523e8e906a7f5886580ad6fb
lynntao1 <66797093+lynntao1@users.noreply.github.com>
2020-12-12 11:54:37 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,17 +1,17 @@
 public class test{
 
-    int key = 0;
-     int pass = 0000;
+   int key = 0;
+   int pass = 0000;
 
-    public test(int k){
-        key = k;
-    }
+   public test(int k){
+      key = k;
+   }
 
-    public int getKey(int p){
-        if(p==pass) {
-            return key;
-        }
+   public int getKey(int p){
+      if(p==pass) {
+         return key;
+      }
         
-        return 1985000;
-    }
+      return 9999999;
+   }
 }
